6ab1a0c85442eb446989b5d17402d69c49d3693b
Matheus Andrade <matheus@tidal.com>
2024-11-30 10:27:49 +0100

    some unit tests

---
+++ forecast-server/src/test/java/com/spond/forecastservice/service/ForecastServiceTest.java
@@ -0,0 +1,92 @@
+package com.spond.forecastservice.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ForecastServiceTest {
+    private static List<Instant> instants;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        Path filePath = Path.of("src/test/resources/compact-time-structure.txt");
+
+        try (Stream<String> lines = Files.lines(filePath)) {
+            instants = lines.map(Instant::parse).toList();
+        }
+    }
+
+    @Test
+    void when_findClosest_hourly_exactMatch_ExactMatchFound() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-11-28T20:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-11-28T20:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_hourly_30mPastHour_RoundsDownToPreviousHour() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-11-28T20:30:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-11-28T20:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_hourly_32PastHour_RoundsUpToNextHour() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-11-28T20:32:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-11-28T21:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_hourlyTo1800_PastOneHour_FindsClosestAvailableTime() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-11-30T19:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-11-30T18:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_hourlyTo1800_PastFourHour_FindsClosestAvailableTime() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-11-30T22:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-01T00:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_every6Hours_exactMatch_FindsExactMatch() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-12-02T12:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-02T12:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_every6Hours_2hourPastInterval_FindsClosestTimeBefore() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-12-02T14:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-02T12:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_every6Hours_3hourPastInterval_FindsClosestTimeBefore() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-12-02T15:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-02T12:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_every6Hours_3hour1MinutePastInterval_FindsClosestAfter() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-12-02T15:01:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-02T18:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_lastDay_FindsRoundingCorrectly() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-12-08T02:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-08T00:00:00Z"));
+    }
+
+    @Test
+    void when_findClosest_lastDay_PastMaxAvailable_FindsMaxAvailableTime() {
+        Instant closestTime = ForecastService.findClosestTime(instants, Instant.parse("2024-12-08T10:00:00Z"));
+        assertThat(closestTime).isEqualTo(Instant.parse("2024-12-08T06:00:00Z"));
+    }
+}
---
+++ forecast-server/src/test/java/com/spond/forecastservice/service/external/MetApiServiceTest.java
@@ -0,0 +1,164 @@
+package com.spond.forecastservice.service.external;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.spond.forecastservice.externalmodel.WeatherData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MetApiServiceTest {
+    @Mock
+    RestTemplate restTemplate;
+
+    @Mock
+    Cache<String, CachedWeatherData> weatherDataCache;
+
+    @InjectMocks
+    MetApiService service;
+
+
+    @Test
+    void when_getForecast_CacheHit_ReturnsFromCache() {
+        double latitude = 10.0;
+        double longitude = 20.0;
+
+        CachedWeatherData cachedData = CachedWeatherData.builder()
+            .data(weatherData())
+            .expiresAt(Instant.now().plusSeconds(60))
+            .build();
+
+        when(weatherDataCache.getIfPresent(anyString())).thenReturn(cachedData);
+
+        ResponseEntity<WeatherData> response = service.getWeatherDataResponseEntity(latitude, longitude);
+
+        //assertions
+        assertThat(HttpStatus.OK).isEqualTo(response.getStatusCode());
+        assertThat(response.getBody()).isNotNull();
+        verify(weatherDataCache, times(1)).getIfPresent(anyString());
+        verifyNoInteractions(restTemplate);
+    }
+
+    @Test
+    void when_getForecast_CacheHitWithExpiredData_MakesDownstreamCall() {
+        String url = mockedUrl();
+        double latitude = 10.0;
+        double longitude = 20.0;
+
+        CachedWeatherData expiredData = CachedWeatherData.builder()
+            .data(weatherData())
+            .expiresAt(Instant.now().minusSeconds(60))
+            .lastModified("Fri, 29 Nov 2024 13:34:26 GMT")
+            .build();
+        when(weatherDataCache.getIfPresent(url)).thenReturn(expiredData);
+        when(restTemplate.exchange(eq(url),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(WeatherData.class))).thenReturn(ResponseEntity.ok(weatherData()));
+
+        ResponseEntity<WeatherData> response = service.getWeatherDataResponseEntity(latitude, longitude);
+
+        //assertions
+        assertThat(HttpStatus.OK).isEqualTo(response.getStatusCode());
+        assertThat(response.getBody()).isNotNull();
+        verify(weatherDataCache, times(1)).getIfPresent(url);
+        verify(restTemplate, times(1)).exchange(eq(url),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(WeatherData.class));
+    }
+
+    @Test
+    void when_getForecast_downstreamNotModified_returnsCachedWeatherData() {
+        String url = mockedUrl();
+        double latitude = 10.0;
+        double longitude = 20.0;
+
+        CachedWeatherData cachedData = CachedWeatherData.builder()
+            .data(weatherData())
+            .expiresAt(Instant.now().minusSeconds(60))
+            .lastModified("Fri, 29 Nov 2024 13:34:26 GMT")
+            .build();
+
+        when(weatherDataCache.getIfPresent(url)).thenReturn(cachedData);
+
+        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<WeatherData> apiResponse = new ResponseEntity<>(null, headers, HttpStatus.NOT_MODIFIED);
+
+        when(restTemplate.exchange(eq(url),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(WeatherData.class))).thenReturn(apiResponse);
+
+        ResponseEntity<WeatherData> response = service.getWeatherDataResponseEntity(latitude, longitude);
+
+        //assertions
+        assertThat(HttpStatus.OK).isEqualTo(response.getStatusCode());
+        assertThat(response.getBody()).isNotNull();
+        verify(weatherDataCache, times(1)).getIfPresent(url);
+        verify(restTemplate, times(1)).exchange(eq(url),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(WeatherData.class));
+        assertThat(response.getBody()).isEqualTo(weatherData());
+    }
+
+
+
+    @Test
+    void when_getForecast_noCacheHit_MakesCallAndUpdateCache() {
+        String url = mockedUrl();
+        double latitude = 10.0;
+        double longitude = 20.0;
+
+        when(weatherDataCache.getIfPresent(url)).thenReturn(null);
+
+        ResponseEntity<WeatherData> apiResponse = ResponseEntity.ok(weatherData());
+        when(restTemplate.exchange(eq(url),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(WeatherData.class))).thenReturn(apiResponse);
+
+        ResponseEntity<WeatherData> response = service.getWeatherDataResponseEntity(latitude, longitude);
+
+        //assertions
+        assertThat(HttpStatus.OK).isEqualTo(response.getStatusCode());
+        assertThat(response.getBody()).isNotNull();
+        verify(weatherDataCache, times(1)).getIfPresent(url);
+        verify(restTemplate, times(1)).exchange(eq(url),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(WeatherData.class));
+        verify(weatherDataCache, times(1)).put(eq(url), any(CachedWeatherData.class));
+    }
+
+    private static String mockedUrl() {
+        return "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=10.000000&lon=20.000000";
+    }
+
+    private static WeatherData weatherData() {
+        return new WeatherData();
+    }
+}
+
+
