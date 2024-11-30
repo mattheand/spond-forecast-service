4662101c79fb1a4c70cff3aca5b88159850e4273
Matheus Andrade <matheus@tidal.com>
2024-11-30 10:23:27 +0100

    small refactoring main class and tests

--- forecast-server/src/main/java/com/spond/forecastservice/SpondForecastServiceApplication.java
+++ forecast-server/src/main/java/com/spond/forecastservice/SpondForecastServiceApplication.java
@@ -1,9 +1,11 @@
-package com.spond.spond_forecast_service;
+package com.spond.forecastservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpondForecastServiceApplication {
 
 	public static void main(String[] args) {
--- forecast-server/src/test/java/com/spond/forecastservice/SpondForecastServiceApplicationTests.java
+++ forecast-server/src/test/java/com/spond/forecastservice/SpondForecastServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spond.spond_forecast_service;
+package com.spond.forecastservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
